60438f909fe45fa6ddce9a7d761b884da98892ad
Jiaxin <ryanli320@outlook.com>
2025-11-06 20:07:09 +0800

    Train on fly

--- modules/config.py
+++ modules/config.py
@@ -10,7 +10,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 @dataclass
 class BasicConfig:
     # Environment
--- runner/main.py
+++ runner/main.py
@@ -1,7 +1,3 @@
-
-from modules.config import load_config
-from pathlib import Path
-
 from runner.run import train_agent
 
 import warnings
@@ -11,6 +7,8 @@ if __name__ == '__main__':
 
     # cfg = load_config(Path.cwd().parent / 'results' / 'LunarLander-v3-20251106' / 'configs' / 'PPO_20251106.yaml')
     # cfg.num_epochs = 7
+    # function below suggested
+    # wrap_config_from_dict(cfg)
     # model_path = Path.cwd().parent / 'results' / 'LunarLander-v3-20251106' / 'models' / 'PPO_epochs_180.pth'
     # train_agent(cfg,model_path)
 
